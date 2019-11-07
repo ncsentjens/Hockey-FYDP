8c375611f33705a49d95f04e93ee3be753d19aa9
Nick Sentjens <nicksentjens@gmail.com>
2019-11-06 22:29:29 -0500

    Initial Setup

---
+++ SmartSnipe/View Controllers/NetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartSnipe
+//
+//  Created by Nick Sentjens on 2019-11-06.
+//  Copyright © 2019 NickSentjens. All rights reserved.
+//
+
+import UIKit
+
+class NetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
